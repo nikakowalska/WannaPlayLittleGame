9498135868c0258c325c13a54a1b543ca4b283a5
nikakowalska <142528082+nikakowalska@users.noreply.github.com>
2023-11-13 22:21:58 +0100

    working on menu

--- src/Gameplay/TeamLeadMenu.java
+++ src/Gameplay/TeamLeadMenu.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class TeamLeadMenu {
     public String teamLeadMenu(List<TeamLeader> teamLeaderList){
-        for (int i = 0; i < ; i++) {
+        for (int i = 1; i <teamLeaderList.size()+1; i++) {
             
         }
         return teamLeaderList.get(1).getName();
